2d040640951fd26bb94b98da1bfaf9f708e1411b
Dae Young Kim <daykim22@gmail.com>
2016-05-03 16:49:41 +0900

    Make NAMESPACE by roxygen

---
+++ R/roxygen_imports.R
@@ -0,0 +1,14 @@
+#' @importFrom plyr mdply llply
+NULL
+
+#' @importFrom lubridate hour minutes hours days
+NULL
+
+#' @importFrom stringr str_replace
+NULL
+
+#' @importFrom devtools install_github
+NULL
+
+#' @import dplyr
+NULL
