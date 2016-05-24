8bc2402ca81410127eb8e532ead8087be65b3608
Dae Young Kim <daykim22@gmail.com>
2016-05-24 23:55:38 +0900

    baseEncored_0.1.10

--- R/handleOptions.R
+++ R/handleOptions.R
@@ -4,6 +4,22 @@ expandSciNotation <- function(value) {
 }
 
 #' @export
+expandDigit <- function(value) {
+  digit.to <- nchar(value)
+  if (grepl("\\.", value))
+    digit.to <- digit.to - 1
+  if (digit.to > 22)
+    digit.to <- 22
+  
+  options(digits = digit.to)
+}
+
+#' @export
+setDigit <- function(digits) {
+  options(digits = digits)
+}
+
+#' @export
 setTimeDigit <- function(digits) {
   options(digits.secs = digits)
 }
