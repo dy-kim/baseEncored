5946ab033c31e57e503811bef14a8de992f4f9f6
Daeyoung Kim <daykim22@gmail.com>
2017-03-03 15:14:56 +0900

    Add `extractFunctionInputFromTest`

---
+++ R/extractFunctionInputFromTest.R
@@ -0,0 +1,83 @@
+#' @export
+extractFunctionInputFromTest <- function(functionName,
+                                         testPkg = ".",
+                                         baseTestCode,
+                                         cache) {
+  cache <- normalizePath(cache)
+  if (!dir.exists(cache)) {
+    stop("Not existing folder.")
+  }
+
+  actualTestCodePath <- getTestCodePath(testPkg, baseTestCode)
+  actualTestCodeName <-
+    tools::file_path_sans_ext(basename(actualTestCodePath))
+  testInputCache <- file.path(cache, actualTestCodeName)
+  if (!dir.exists(testInputCache)) {
+    dir.create(testInputCache, recursive = TRUE)
+  }
+
+  f <- match.fun(functionName)
+  if (!isFunctionInputCaptured(f, testInputCache)) {
+    printTestStartInstruction(testInputCache)
+    stop("Please follow above information.")
+  }
+
+  if (!isFuncEnvironFileExisting(testInputCache, functionName)) {
+    setTargetedFunction(functionName, optionName = SAVE_FUNC_ENVIRON)
+    testthat::test_file(path = actualTestCodePath, reporter = "list")
+    unsetTargetedFunction(functionName, optionName = SAVE_FUNC_ENVIRON)
+  }
+
+  if (!isFuncEnvironFileExisting(testInputCache, functionName)) {
+    FORCE_FATAL("Function %s is not in the test %s.R!",
+                functionName,
+                actualTestCodeName)
+  }
+
+  envFilePath <- findFuncEnvironFile(testInputCache, functionName)
+  return(envFilePath)
+}
+
+#' @importFrom testthat find_test_scripts
+getTestCodePath <- function(testPkg, baseTestCode) {
+  path <-
+    testthat::find_test_scripts(path = file.path(testPkg, "tests", "testthat"),
+                                filter = baseTestCode)
+  stopifnot(length(path) == 1L)
+  return(path)
+}
+
+isFunctionInputCaptured <- function(f, testInputCache) {
+  answer <-
+    getFunctionInputCapturingCode(testInputCache) %>%
+    removeAllWhitespace()
+  test <- getFirstLineOfFunction(f) %>% removeAllWhitespace()
+  return(identical(answer, test))
+}
+
+getFunctionInputCapturingCode <- function(testInputCache) {
+  sprintf("baseEncored::saveFunctionEnvironment(savePath = \"%s\")",
+          testInputCache)
+}
+
+#' @importFrom stringr str_replace_all
+removeAllWhitespace <- function(x) {
+  stringr::str_replace_all(x, " ", "")
+}
+
+printTestStartInstruction <- function(testInputCache) {
+  FORCE_INFO("Insert following line to the first line of the function definition.")
+  FORCE_INFO(getFunctionInputCapturingCode(testInputCache))
+}
+
+getFirstLineOfFunction <- function(f) {
+  funcDefinition <- deparse(f)
+  firstLineIndex <-
+    min(findIndexOfLeftCurlyBraces(funcDefinition)) + 1L
+
+  return(funcDefinition[firstLineIndex])
+}
+
+findIndexOfLeftCurlyBraces <- function(x) {
+  grep(pattern = "\\{", x = x)
+}
